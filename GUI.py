0b575853ecb9184d77784a3ae81358c0bafd14f4
SoloKinevil <Keener.phoenix@gmail.com>
2025-02-06 17:12:15 -0500

    Update GUI.py

---
+++ GUI.py
@@ -0,0 +1,18 @@
+'''
+CIS 350 - Dungeons and Dragons Companion App
+Shayan, Phoenix and Ian
+
+This file contains the constructors for the GUI. The GUI will allow user input to export and input character
+data as well as specific inventories and notes.
+'''
+
+import tkinter as tk
+from tkinter import *
+
+class GUI():
+    def __init__(self):
+        self.root = tk.Tk()
+
+# class export()
+
+# class import()
