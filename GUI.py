c34f3cb1acfa304ac03703f560904f7c05fe8991
SoloKinevil <Keener.phoenix@gmail.com>
2025-02-12 10:44:09 -0500

    GUI NOTES METHOD
    Added functionality to run GUI and Open a "Notepad" that will be saved between opening and reopening the file if saved by pressing return. GRID formatting next

--- GUI.py
+++ GUI.py
@@ -9,10 +9,51 @@ data as well as specific inventories and notes.
 import tkinter as tk
 from tkinter import *
 
-class GUI():
-    def __init__(self):
-        self.root = tk.Tk()
+class GUI(tk.Frame):
+    # Constructor for user GUI and format
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+        # Start application
+        self.run()
+
+    def create_widgets(self):
+        # Initialize all widgets and labels
+
+        # ENTRY WIDGET
+        # Read in saved_contents to updated user notes
+        f = open('saved_contents.txt', 'r')
+        self.saved_contents = f.read()
+        f.close()
+
+        # Initialize entry values for NOTES
+        self.entry = tk.Entry()
+        self.contents = tk.StringVar()
+        self.contents.set(self.saved_contents)
+        self.entry.pack()
+
+    def run(self):
+
+        # Tell the entry widget to watch this variable.
+        self.entry["textvariable"] = self.contents
+
+        # Saves new user input to text document when return it pressed
+        self.entry.bind('<Key-Return>', self.save_contents)
+
+    def save_contents(self, *args):
+        # Function to save users stored information
+        self.saved_contents = self.contents.get()
+        f = open('saved_contents.txt', 'w')
+        f.write(self.saved_contents)
+        f.close()
 
 # class export()
 
 # class import()
+
+root = tk.Tk()
+myapp = GUI(root)
+myapp.mainloop()
