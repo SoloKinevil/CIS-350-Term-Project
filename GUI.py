dd0412274a6945962e314ba34363a8e68a3b9968
SoloKinevil <Keener.phoenix@gmail.com>
2025-02-24 10:24:59 -0500

    Editing Character
    Editing saving of character notes tab such that each field is saved and recompiled

--- GUI.py
+++ GUI.py
@@ -18,53 +18,111 @@ class GUI(tk.Frame):
         super().__init__(master)
         self.pack()
 
-        notebook = ttk.Notebook(self, height = 300, width = 300)
+        # Create notebook and format
+        notebook = ttk.Notebook(self, height = 450, width = 300)
         notebook.pack(expand = True, fill = 'both')
+
+        #Add notebook tab with roll function
         dice_roll_tab = Dice_roll_tab(notebook)
-        notes = Notes_tab(notebook)
         notebook.add(dice_roll_tab, text='Dice Roll')
-        notebook.add(notes, text='Character',)
+
+        # Add notebook tab with Notes functionality
+        notes = Notes_tab(notebook)
+        notebook.add(notes, text='Active Character', )
+
+
 
 
 # This Class initializes the Notes tab where the fields are saved
 class Notes_tab(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
-        # ENTRY WIDGET
+
         # Read in saved_contents to updated user notes
         f = open('saved_contents.txt', 'r')
         self.saved_contents = f.read()
         f.close()
 
-        self.label2 = tk.Label(self, text="Notes")
-        self.label2.pack()
+        # Label for Name
+        self.label = ttk.Label(self, text = "Character Name")
+        self.label.pack()
+        # Initialize Entry values for Name
+        self.name = tk.Entry(self, width=25)  # Starts tk entry field
+        self.name_contents = tk.StringVar()
+        self.name_contents.set(self.saved_contents)  # Sets the entry field to previous name
+        self.name.pack()
 
+        # Label for Race
+        self.label2 = ttk.Label(self, text="Race")
+        self.label2.pack()
+        # Initialize Entry values for Race
+        self.race = tk.Entry(self, width=25)  # Starts tk entry field
+        self.race_contents = tk.StringVar()
+        self.race_contents.set(self.saved_contents)  # Sets the entry field to previous race
+        self.race.pack()
+
+        # Label for Class
+        self.label3 = ttk.Label(self, text="Class")
+        self.label3.pack()
+        # Initialize Entry values for class
+        self.Class = tk.Entry(self, width=25)  # Starts tk entry field
+        self.Class_contents = tk.StringVar()
+        self.Class_contents.set(self.saved_contents)  # Sets the entry field to previous class
+        self.Class.pack()
+
+        # Label for Dex Modifier ##### USED FOR DICE ROLL
+        self.Dex = ttk.Label(self, text="Class")
+        self.Dex.pack()
+        # Initialize Entry values for Dex
+        self.dex = tk.Entry(self, width=25)  # Starts tk entry field
+        self.dex_contents = tk.StringVar()
+        self.dex_contents.set(self.saved_contents)  # Sets the entry field to previous Dex
+        self.dex.pack()
+
+        # Label for Notes
+        self.label4 = tk.Label(self, text="Notes")
+        self.label4.pack()
         # Initialize Entry values for NOTES
         self.notes = tk.Entry(self ,width = 100)  # Starts tk entry field
-
-        self.contents = tk.StringVar()
-        self.contents.set(self.saved_contents)  # Sets the entry field to previous notes
+        self.note_contents = tk.StringVar()
+        self.note_contents.set(self.saved_contents)  # Sets the entry field to previous notes
         self.notes.pack()
 
+        # Follows text changes within notebook
+        self.notes["textvariable"] = self.note_contents
+        self.dex["textvariable"] = self.dex_contents
+        self.Class["textvariable"] = self.Class_contents
+        self.race["textvariable"] = self.race_contents
+        self.name["textvariable"] = self.name_contents
+
+        # Creates import button
         open_button = tk.Button(self, text="Import Data", command=self.open_file)
         open_button.pack(pady=20)
-        
-        # Widget follows changes
-        self.notes["textvariable"] = self.contents
 
-        # Saves new user changes to text document when return it pressed
-        self.notes.bind('<Key-Return>', self.save_contents)
+        # Saves new user changes to text document when clicked
+        save_button = tk.Button(self, text="Save Data", command=self.save_contents)
+        save_button.pack(pady=20)
 
     # This function saves the users stored information "Export"
     def save_contents(self, *args):
-        self.saved_contents = self.contents.get()
         f = open('saved_contents.txt', 'w')
-        f.write(self.saved_contents)
+        f.write(self.name_contents.get())
+        f.write("/n")
+        f.write(self.race_contents.get())
+        f.write("/n")
+        f.write(self.Class_contents.get())
+        f.write("/n")
+        f.write(self.dex_contents.get())
+        f.write("/n")
+        f.write(self.note_contents.get())
         f.close()
 
+    # Allows user to upload text document to input into application
     def open_file(self):
+        # Ask user for filepath through file explorer
         filepath = filedialog.askopenfilename()
         while filepath:
+            # Ensure file is .txt
             if filepath.endswith('.txt'):
                 print("Opening Text File: " + filepath)
                 with open(filepath, 'r') as incoming, open('saved_contents.txt', 'w') as Characterdata:
@@ -99,12 +157,8 @@ class Dice_roll_tab(ttk.Frame):
         self.label2.config(text=f"Roll: {result}")
 
 
-root = tk.Tk()
-root.title("D&D Companion App")
-
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=1)
-root.rowconfigure(0, weight=1)
-
-myapp = GUI(root)
-myapp.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("D&D Companion App")
+    myapp = GUI(root)
+    myapp.mainloop()
